cbd22b18c3ac61b11ed00feac174fc6f3a2c3622
Ciszek1 <61918655+Ciszek1@users.noreply.github.com>
2022-03-14 00:12:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require('express')
 const app = express()
-const fetch = require('node-fetch');
 const port = 3000
 
 app.get('/', (req, res) => {
 
-  const lessonAll = [["Basen, Basen, Angielski, Wos, Matematyke, Matematyke, Biologie, Polski", "siudmej czterdzieści pięć", "pietnastej trzydzieści"], {}];
-  const weatherApi = "http://api.openweathermap.org/data/2.5/weather?lat=54.3294106&lon=18.593441&appid=50d25bbab013b3251462733786b007d7";
+  const lessonAll = [["Basen, Basen, Angielski, Wos, Matematyke, Matematyke, Biologie, Polski", "siudmej czterdzieści pięć", "pietnastej trzydzieści"], []];
   const dayName = new Date().toLocaleString('pl-pl', { weekday: 'long' });
   const d = new Date();
   let day = d.getDay();
-
-  //_______________________Pogoda
-  fetch('https://api.openweathermap.org/data/2.5/weather?lat=54.3294106&lon=18.593441&appid=50d25bbab013b3251462733786b007d7')    
-    .then(res => res.json())
-    .then((json) => {
-    const api = json;
-
 
 
       //_______________________Lesson
@@ -26,14 +17,12 @@ app.get('/', (req, res) => {
       }
       const lessonOut = `Lekcje zaczynasz o ${lessonAll[lessonDayNumber][1]}, masz: ${lessonAll[lessonDayNumber][0]} kończysz o ${lessonAll[lessonDayNumber][2]} `;
 
-      const pogoda = `W Gdansku jest ${Number(Math.round(api.main.temp - 273.15 + 'e+2') + 'e-2')} stopni celcusza, wilgotność powietrza wynosi ${api.main.humidity} procent `
 
-      //________________________Output
-      var text = `WItaj Łukasz, dziś jest ${dayName}. ${pogoda}. ${lessonOut}. Miłego dnia!`;
+//________________________Output
+var text = `WItaj Łukasz, dziś jest ${dayName}. ${lessonOut}. Miłego dnia!`;
 
 
-      res.send(text)
-    });
+res.send(text)
 
 })
 
